dd1fa76c3e1f90e77cb07cf8a11ed3d3fe3d9a5b
felix <felix@zaslavskiy.net>
2023-03-27 00:45:17 -0400

    first try format_chat

--- format_chat.py
+++ format_chat.py
@@ -37,10 +37,62 @@ text text text text.
 
 Becomes:
 > **ChatGPT**
+>
 > Text text text.
 >
 > text text text text
 
-
+Note the extra > after the > **ChatGPT** line
 
 """
+
+import sys
+import os
+
+def process_file(filename):
+    try:
+        with open(filename, "r") as file:
+            lines = file.readlines()
+
+        output_lines = []
+        inside_blockquote = False
+        header = True
+        for line in lines:
+            if header:
+                output_lines.append(line)
+                if line.startswith("> **"):
+                    header = False
+                    output_lines.append(">\n")
+                    inside_blockquote = True
+            elif line.startswith("> **"):
+                output_lines.append(line.rstrip() + "\n>\n")
+                inside_blockquote = True
+            elif line.strip() == "":
+                output_lines.append(line)
+                inside_blockquote = False
+            else:
+                if inside_blockquote:
+                    output_lines.append("> " + line)
+                else:
+                    output_lines.append(line)
+
+        with open(filename, "w") as output_file:
+            output_file.writelines(output_lines)
+
+        print(f"Markdown file updated: {filename}")
+
+    except FileNotFoundError:
+        print(f"Error: {filename} not found.")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py filename")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    if not os.path.exists(filename):
+        print(f"Error: {filename} not found.")
+        sys.exit(1)
+
+    process_file(filename)
